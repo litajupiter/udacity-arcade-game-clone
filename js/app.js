e126aa0d97da7e96eb24cc2fce0e64bc33dbb932
Brianne <brianne.hennel@gmail.com>
2018-06-30 17:58:12 -0400

    Win modal displays congratulations text and number of lives remaining

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var numLives = 3;
+
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here, we've provided one for you to get started
@@ -72,6 +74,7 @@ Player.prototype.handleInput = function(key) {
     }
     // If the player reaches the water, the game should be reset by moving the player back to the initial location
     if (this.y < 0) {
+      winGame();
       player.x = 203;
       player.y = 403;
     }
@@ -95,3 +98,29 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function winGame() {
+  modal.classList.toggle("visible");
+  document.querySelector(".modal-text").innerHTML = `Congratulations, you won! With ${numLives} out of 3 lives remaining!`;
+}
